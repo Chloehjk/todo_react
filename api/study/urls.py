f3145fc1b04411396b7a0598f1ca1a45f1d546bf
Kim Hyunjin <1605hjk@gmail.com>
2020-09-15 18:32:46 +0900

    viewset practice code

--- api/study/urls.py
+++ api/study/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('students', views.StudentView)
+router.register('scores', views.ScoreView)
+
 urlpatterns = [
-    path('students', views.StudentView.as_view()),
-    path('students/<pk>', views.StudentDetailView.as_view()),
-    path('scores', views.ScoreView.as_view()),
-    path('scores/<pk>', views.ScoreDetailView.as_view()),
+    path('', include(router.urls)),
+    
+    ##########CBV############
+    # path('students', views.StudentView.as_view()),
+    # path('students/<pk>', views.StudentDetailView.as_view()),
+    # path('scores', views.ScoreView.as_view()),
+    # path('scores/<pk>', views.ScoreDetailView.as_view()),
+    ##########FBV############
     # path('students/', views.StudentView),
     # path('students/<id>', views.StudentDetailView),
     # path('scores/', views.ScoreView),
--- api/study/views.py
+++ api/study/views.py
@@ -1,84 +1,156 @@
 from django.shortcuts import render, get_object_or_404
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import status, viewsets
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.decorators import api_view, action
 from rest_framework.views import APIView
 from .models import Students, Scores
 from .serializers import StudentSerializer, ScoreSerializer
 from rest_framework.response import Response
 
-class StudentView(APIView):
 
-    def get(self, request):
-        qs = Students.objects.all()
-        serializer = StudentSerializer(qs, many=True)
+class StudentView(ModelViewSet):
+
+    queryset = Students.objects.all()
+    serializer_class = StudentSerializer
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        name = self.request.query_params.get('name')
+        if name:
+            qs = qs.filter(name=name)
+        return qs
+
+    @action(detail=False, methods=['GET'])
+    def incheon(self, request):
+        qs = self.get_queryset().filter(address__contains='인천') #like %인천%
+        serializer = self.get_serializer(qs, many=True)
         return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
 
-class StudentDetailView(APIView):
 
-    def get_object(self, pk):
-        return get_object_or_404(Students, pk=pk)
-    
-    def get(self, request, pk):
-        qs = self.get_object(pk)
-        serializer = StudentSerializer(qs)
+    @action(detail=True, methods=['PUT'])
+    def init(self, request, pk):
+        instance = self.get_object()  
+        instance.address = ''
+        instance.email = ''
+        instance.save(update_fields=['address', 'email'])
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)  
+
+
+
+class ScoreView(ModelViewSet):
+    queryset = Scores.objects.all()
+    serializer_class = ScoreSerializer
+
+    def get_queryset(self):
+        #Scores.objects.all()
+        qs = super().get_queryset()
+        name = self.request.query_params.get('name')
+        math = self.request.query_params.get('math')
+        english = self.request.query_params.get('english')
+        science = self.request.query_params.get('scince')
+        order = self.request.query_params.get('order')
+        if name:
+            qs = qs.filter(name=name)
+        if math:
+            qs = qs.filter(math__gte = math)
+        if english:
+            qs = qs.filter(english__gte = english)
+        if science:
+            qs = qs.filter(science__gte = science)
+        if order:
+            qs = qs.order_by(order)
+        return qs
+
+
+# ###############합 270점 이상##############################
+##g함수명이 주소가 됨
+    @action(detail=False, methods=['GET'])
+    def top(self, request):
+        qs = self.get_queryset().filter(math__gte=80, english__gte=80, science__gte=80)
+        serializer = self.get_serializer(qs, many=True)
         return Response(serializer.data)
+
+
+#######################CBV로 만든거############################
+
+# class StudentView(APIView):
+
+#     def get(self, request):
+#         qs = Students.objects.all()
+#         serializer = StudentSerializer(qs, many=True)
+#         return Response(serializer.data)
     
-    def put(self, request, pk):
-        qs = self.get_object(pk)
-        serializer = StudentSerializer(qs, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+#     def post(self, request):
+#         serializer = StudentSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
 
-    def delete(self, request, pk):
-        qs = self.get_object(pk)
-        qs.delete()
-        return Response(status=204)
+# class StudentDetailView(APIView):
 
-
-class ScoreView(APIView):
-    def get(self, request):
-        score = Scores.objects.all()
-        serializer = ScoreSerializer(score, many=True)
-        return Response(serializer.data)
+#     def get_object(self, pk):
+#         return get_object_or_404(Students, pk=pk)
     
-    def post(self, request):
-        serializer = ScoreSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-
-class ScoreDetailView(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(Scores, pk=pk)
-
-    def get(self, request, pk):
-        score = self.get_object(pk)
-        serializer = ScoreSerializer(score)
-        return Response(serializer.data)
+#     def get(self, request, pk):
+#         qs = self.get_object(pk)
+#         serializer = StudentSerializer(qs)
+#         return Response(serializer.data)
     
-    def put(self, request, pk):
-        score = self.get_object(pk)
-        serializer = ScoreSerializer(score, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+#     def put(self, request, pk):
+#         qs = self.get_object(pk)
+#         serializer = StudentSerializer(qs, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=400)
 
-    def delete(self, request, pk):
-        score = self.get_object(pk)
-        score.delete()
-        return Response(status=204)
+#     def delete(self, request, pk):
+#         qs = self.get_object(pk)
+#         qs.delete()
+#         return Response(status=204)
+
+
+# class ScoreView(APIView):
+#     def get(self, request):
+#         score = Scores.objects.all()
+#         serializer = ScoreSerializer(score, many=True)
+#         return Response(serializer.data)
+    
+#     def post(self, request):
+#         serializer = ScoreSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
+
+
+# class ScoreDetailView(APIView):
+#     def get_object(self, pk):
+#         return get_object_or_404(Scores, pk=pk)
+
+#     def get(self, request, pk):
+#         score = self.get_object(pk)
+#         serializer = ScoreSerializer(score)
+#         return Response(serializer.data)
+    
+#     def put(self, request, pk):
+#         score = self.get_object(pk)
+#         serializer = ScoreSerializer(score, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=400)
+
+#     def delete(self, request, pk):
+#         score = self.get_object(pk)
+#         score.delete()
+#         return Response(status=204)
+
+
+
+#######################FBV로 만든거############################
 
 # @api_view(['GET', 'POST'])
 # def StudentView(request):
